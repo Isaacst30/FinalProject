c65b1105dee2e50f509559960617709d0ff53ae6
Isaacst30 <keith.stanek@okwustudents.edu>
2024-04-17 23:34:32 -0500

    I added info and deleted a file because it was not needed, but this project isn't finished and still needs the API

---
+++ util.js
@@ -0,0 +1,40 @@
+charities = [
+      {
+        "name": "St. Jude Children's Research Hospital",
+        "description": "St. Jude is leading the way the world understands, treats, and defeats childhood cancer and other life-threatening diseases.",
+        "mission": "Advance cures, and means of prevention, for pediatric catastrophic diseases through research and treatment.",
+        "impact": "St. Jude has helped push the overall childhood cancer survival rate from 20% to more than 80% since it opened more than 50 years ago.",
+        "opportunities_to_get_involved": "<ul> <li>Donate </li> <li>Volunteer</li> <li>Participate in fundraising events</li></ul>",
+        "contact_information": {
+          "address": "262 Danny Thomas Place, Memphis, TN 38105, USA",
+          "phone": "1-800-822-6344",
+          "email": "donate@stjude.org"
+        }
+      },
+      {
+        "name": "World Wildlife Fund (WWF)",
+        "description": "WWF is the world's leading independent conservation organization.",
+        "mission": "To conserve nature and reduce the most pressing threats to the diversity of life on Earth.",
+        "impact": "WWF's efforts have helped save over 16,000 species from extinction since 1961.",
+        "opportunities_to_get_involved": "<ul><li>Donate</li> <li>Adopt an animal</li> <li>Become a member</li> <li>Volunteer</li></ul>",
+        "contact_information": {
+          "address": "1250 24th Street, NW Washington, DC 20037, USA",
+          "phone": "1-202-293-4800",
+          "email": "panda@wwfus.org"
+        }
+      },
+      {
+        "name": "International Committee of the Red Cross (ICRC)",
+        "description": "The ICRC is a humanitarian institution that works to protect and assist victims of armed conflict and other situations of violence.",
+        "mission": "To ensure respect for the Geneva Conventions and to provide protection and assistance to victims of armed conflict and other situations of violence.",
+        "impact": "The ICRC operates worldwide, helping people affected by conflict and armed violence and promoting the laws that protect victims of war.",
+        "opportunities_to_get_involved": "<ul><li>Donate</li> <li>Become a member</li> <li>Volunteer</li> <li>Work for ICRC</li></ul>",
+        "contact_information": {
+          "address": "19 Avenue de la Paix, 1202 Geneva, Switzerland",
+          "phone": "+41 22 734 60 01",
+          "email": "info@icrc.org"
+        }
+      }
+    ]
+  
+  
